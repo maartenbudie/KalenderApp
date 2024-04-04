33d900c0ad85b92e58315d7ee20c0aec3d3785f6
maartenbudie <i529302@fhict.nl>
2024-04-04 14:29:32 +0200

    Made days and current month dynamic

--- KalenderApp/wwwroot/js/site.js
+++ KalenderApp/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-// Write your JavaScript code.
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".days");
+
+let date = new Date(),
+currentYear= date.getFullYear(),
+currentMonth = date.getMonth();
+
+const renderCalender = () => {
+    let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0 ).getDate();
+    let liTag = "";
+
+    for(let i = 1; i < lastDateOfMonth; i++){
+        liTag += `<li>${i}</li>`;
+    }
+
+    currentDate.innerText = `${months[currentMonth]} ${currentYear}`;
+    daysTag.innerHTML = liTag;
+}
+renderCalender();
