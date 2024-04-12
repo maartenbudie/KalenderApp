19b5a72b9aaac07844e9083e1a4d244aba3e65b8
maartenbudie <i529302@fhict.nl>
2024-04-12 09:43:50 +0200

    Added testing query

--- KalenderApp.DAL/EventData.cs
+++ KalenderApp.DAL/EventData.cs
@@ -2,6 +2,37 @@
 {
     public class EventData
     {
+        private int id;
+        private int organiserId;
+        private string name;
+        private DateTime startTime;
+        private DateTime endTime;
+        private string? location;
+        private Repetition? repetition;
         
+        public List<int> getAllEventIDs()
+        {
+            List<int> eventIDs = new List<int>();
+
+            try{
+                using(SqlConnection connection = new SqlConnection(DatabaseClass.connectionString))
+                {
+                    string query = "SELECT id FROM event";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()){
+                        id = Convert.ToInt32(reader["ID"]);
+                        eventIDs.Add(id);
+                    }
+                    reader.Close();
+                }
+            }
+            catch{
+
+            }
+            return eventIDs;
+        }
     }
 }
