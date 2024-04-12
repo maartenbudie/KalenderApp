acc67a1d120449626a38da94f3e26f4dbde3d3f1
maartenbudie <i529302@fhict.nl>
2024-04-12 12:48:48 +0200

    Added UserData and IUserData

---
+++ KalenderApp.DAL/IUserData.cs
@@ -0,0 +1,5 @@
+namespace KalenderApp.DAL;
+
+public interface IUserData
+{
+}
---
+++ KalenderApp.DAL/UserData.cs
@@ -0,0 +1,9 @@
+namespace KalenderApp.DAL;
+
+public class UserData
+{
+    private int id;
+    private string? username;
+    private string? email;
+    private string? password;
+}
