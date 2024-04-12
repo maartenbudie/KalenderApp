8cf6520cb7968c3f05f7ec6b966be64a5a5cd99f
maartenbudie <i529302@fhict.nl>
2024-04-12 09:37:28 +0200

    Added DatabaseClass with connectionString

---
+++ KalenderApp.DAL/DatabaseClass.cs
@@ -0,0 +1,7 @@
+namespace KalenderApp.DAL
+{
+    public static class DatabaseClass
+    {
+        public const string connectionString = "Data Source=localhost; Initial Catalog=kalenderappdata; Integrated Security=True";
+    }
+}
