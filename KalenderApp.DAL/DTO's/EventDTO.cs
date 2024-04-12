710c8156e86e06caf98c7a1f51f97654092e27be
maartenbudie <i529302@fhict.nl>
2024-04-12 13:14:06 +0200

    Changed namespace declaration to be consistent with other objects

--- KalenderApp.DAL/DTO's/EventDTO.cs
+++ KalenderApp.DAL/DTO's/EventDTO.cs
@@ -1,24 +1,23 @@
-namespace KalenderApp.DAL
-{
-    public abstract class EventDTO
-    {
-        public int id {get; private set;}
-        public int organiserId {get; private set;}
-        public string name {get; private set;}
-        public DateTime startTime {get; private set;}
-        public DateTime endTime {get; private set;}
-        public string? location {get; private set;}
-        public Repetition? repetition {get; private set;}
+namespace KalenderApp.DAL;
 
-        public EventDTO(int id, int organiserId, string name, DateTime startTime, DateTime endTime, string? location, Repetition? repetition)
-        {
-            this.id = id;
-            this.organiserId = organiserId;
-            this.name = name;
-            this.startTime = startTime;
-            this.endTime = endTime;
-            this.location = location;
-            this.repetition = repetition;
-        }
+public abstract class EventDTO
+{
+    public int id { get; private set; }
+    public int organiserId { get; private set; }
+    public string name { get; private set; }
+    public DateTime startTime { get; private set; }
+    public DateTime endTime { get; private set; }
+    public string? location { get; private set; }
+    public Repetition? repetition { get; private set; }
+
+    public EventDTO(int id, int organiserId, string name, DateTime startTime, DateTime endTime, string? location, Repetition? repetition)
+    {
+        this.id = id;
+        this.organiserId = organiserId;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.repetition = repetition;
     }
 }
