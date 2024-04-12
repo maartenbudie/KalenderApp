77f18c32e189e9757034202e1fdea32fe9a73dfc
maartenbudie <i529302@fhict.nl>
2024-04-12 09:25:40 +0200

    Made Event DTO fields public, made location and repetition nullable, and added constructor

--- KalenderApp.DAL/EventDTO.cs
+++ KalenderApp.DAL/EventDTO.cs
@@ -2,12 +2,23 @@ namespace KalenderApp.DAL
 {
     public abstract class EventDTO
     {
-        public int id;
-        public int organiserId;
-        private string name;
-        private DateTime startTime;
-        private DateTime endTime;
-        private string location;
-        private Repetition repetition;
+        public int id {get; private set;}
+        public int organiserId {get; private set;}
+        public string name {get; private set;}
+        public DateTime startTime {get; private set;}
+        public DateTime endTime {get; private set;}
+        public string? location {get; private set;}
+        public Repetition? repetition {get; private set;}
+
+        public EventDTO(int id, int organiserId, string name, DateTime startTime, DateTime endTime, string? location, Repetition? repetition)
+        {
+            this.id = id;
+            this.organiserId = organiserId;
+            this.name = name;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.location = location;
+            this.repetition = repetition;
+        }
     }
 }
