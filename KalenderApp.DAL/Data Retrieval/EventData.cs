49b58d339d799e61b7849f03539d208361df9d9d
maartenbudie <i529302@fhict.nl>
2024-04-12 13:04:06 +0200

    Added Event objects

---
+++ KalenderApp.DAL/Data Retrieval/EventData.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+using KalenderApp.Core;
+
+namespace KalenderApp.DAL
+{
+    public class EventData : IEventData
+    {
+        public List<int> getAllEventIDs()
+        {
+            int id;
+            List<int> eventIDs = new List<int>();
+
+            try{
+                using(SqlConnection connection = new SqlConnection(DatabaseClass.connectionString))
+                {
+                    string query = "SELECT id FROM event";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()){
+                        id = Convert.ToInt32(reader["ID"]);
+                        eventIDs.Add(id);
+                    }
+                    reader.Close();
+                }
+            }
+            catch{
+                Console.WriteLine("Error retrieving data");
+            }
+            return eventIDs;
+        }
+    }
+}
