03746acd5b8de0a9befa55509347cfed5e122bf5
maartenbudie <147500722+maartenbudie@users.noreply.github.com>
2024-04-21 11:32:41 +0200

    Renamed EventLogic to EventService

---
+++ KalenderApp.Core/Logic/EventService.cs
@@ -0,0 +1,20 @@
+namespace KalenderApp.Core;
+
+public class EventService
+{
+    private int id;
+    private int organiserId;
+    private string? name;
+    private DateTime startTime;
+    private DateTime endTime;
+    private string? location;
+    private Repetition? repetition;
+
+    public EventService(){
+    }
+
+    public List<EventDTO> getEventsForDay(IEventData eventData, DateTime dateTime)
+    {
+        return eventData.getEventsForDay();
+    }
+}
