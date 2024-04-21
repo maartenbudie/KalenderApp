b5681d65daddc5b70bb1c4f1f918a4299d0e97c8
maartenbudie <147500722+maartenbudie@users.noreply.github.com>
2024-04-21 11:41:50 +0200

    Made getEventsForDay functional with parameterised query to prevent injection

--- KalenderApp.DAL/Data Retrieval/EventData.cs
+++ KalenderApp.DAL/Data Retrieval/EventData.cs
@@ -5,50 +5,40 @@ namespace KalenderApp.DAL
 {
     public class EventData : IEventData
     {
-        public List<EventDTO> getEventsForDay()
+        public List<EventDTO> getEventsForDay(DateTime dateTime)
         {
             List<EventDTO> events = new List<EventDTO>();
 
+            dateTime = dateTime.Date;
+
             try
             {
-                // using (SqlConnection connection = new SqlConnection(DatabaseClass.connectionString))
-                // {
-                //     string query = "SELECT * FROM event";
-                //     SqlCommand command = new SqlCommand(query, connection);
-                //     connection.Open();
-                //     SqlDataReader reader = command.ExecuteReader();
-
-                //     while (reader.Read())
-                //     {
-                //         // int id = Convert.ToInt32(reader["id"]);
-                //         // int category_id = Convert.ToInt32(reader["category_id"]);
-                //         // string name = Convert.ToString(reader["name"]);
-                //         // DateTime start_time = DateTime.Parse(Convert.ToString(reader["start_time"]));
-                //         // DateTime end_time = DateTime.Parse(Convert.ToString(reader["end_time"]));
-                //         // string location = Convert.ToString(reader["location"]);
-                //         // Repetition repetition = (Repetition)Enum.Parse(typeof(string), Convert.ToString(reader["repetition"]), true);
-
-                //         // EventDTO eventDTO = new EventDTO(id, category_id, name, start_time, end_time, location, repetition);
-                //         // events.Add(eventDTO);
-                //     }
-                //     reader.Close();
-                // }
+                using (SqlConnection connection = new SqlConnection(DatabaseClass.connectionString))
+                {
+                    string query = $"SELECT * FROM event WHERE CONVERT(date, start_time) = @dateTime";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@dateTime", dateTime);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();                 
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["id"]);
+                        int category_id = Convert.ToInt32(reader["category_id"]);
+                        string name = Convert.ToString(reader["name"]);
+                        DateTime start_time = DateTime.Parse(Convert.ToString(reader["start_time"]));
+                        DateTime end_time = DateTime.Parse(Convert.ToString(reader["end_time"]));
+                        string location = Convert.ToString(reader["location"]);
+                        Repetition repetition = (Repetition)Enum.Parse(typeof(string), Convert.ToString(reader["repetition"]), true);                        
+                        EventDTO eventDTO = new EventDTO(id, category_id, name, start_time, end_time, location, repetition);
+                        events.Add(eventDTO);
+                    }
+                    reader.Close();
+                }
             }
             catch
             {
                 Console.WriteLine("Error retrieving data");
             }
-
-            int id = 1;//Convert.ToInt32(reader["id"]);
-            int category_id = 1;//Convert.ToInt32(reader["category_id"]);
-            string name = "test";//Convert.ToString(reader["name"]);
-            DateTime start_time = DateTime.Now;//DateTime.Parse(Convert.ToString(reader["start_time"]));
-            DateTime end_time = DateTime.Now;//DateTime.Parse(Convert.ToString(reader["end_time"]));
-            string location = "here";//Convert.ToString(reader["location"]);
-            Repetition repetition = Repetition.None;//(Repetition)Enum.Parse(typeof(string), Convert.ToString(reader["repetition"]), true);
-
-            EventDTO eventDTO = new EventDTO(id, category_id, name, start_time, end_time, location, repetition);
-            events.Add(eventDTO);
             return events;
         }
     }
