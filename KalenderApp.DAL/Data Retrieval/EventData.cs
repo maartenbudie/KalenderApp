6d8b944343a7a88287b9c01862186f73fdf81e16
maartenbudie <i529302@fhict.nl>
2024-05-21 11:49:42 +0200

    Further improved on the error handling and added a using block

--- KalenderApp.DAL/Data Retrieval/EventData.cs
+++ KalenderApp.DAL/Data Retrieval/EventData.cs
@@ -17,30 +17,33 @@ namespace KalenderApp.DAL
                     command.Parameters.AddWithValue("@dateTime", dateTime);
                     connection.Open();
                     MySqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
+                    using (reader)
                     {
-                        int id = Convert.ToInt32(reader["id"]);
-                        int category_id = Convert.ToInt32(reader["category_id"]);
-                        string name = Convert.ToString(reader["name"]);
-                        DateTime start_time = DateTime.Parse(Convert.ToString(reader["start_time"]));
-                        DateTime end_time = DateTime.Parse(Convert.ToString(reader["end_time"]));
-                        string location = Convert.ToString(reader["location"]);
-                        string repetition = Convert.ToString(reader["repetition"]);
+                        while (reader.Read())
+                        {
+                            int id = Convert.ToInt32(reader["id"]);
+                            int category_id = Convert.ToInt32(reader["category_id"]);
+                            string name = Convert.ToString(reader["name"]);
+                            DateTime start_time = DateTime.Parse(Convert.ToString(reader["start_time"]));
+                            DateTime end_time = DateTime.Parse(Convert.ToString(reader["end_time"]));
+                            string location = Convert.ToString(reader["location"]);
+                            string repetition = Convert.ToString(reader["repetition"]);
 
-                        EventDTO eventDTO = new EventDTO(id, category_id, name, start_time, end_time, location, repetition);
-                        events.Add(eventDTO);
+                            EventDTO eventDTO = new EventDTO(id, category_id, name, start_time, end_time, location, repetition);
+                            events.Add(eventDTO);
+                        }
                     }
-                    reader.Close();
                 }
-                return events;  
+                return events;
             }
-            catch(MySqlException ex)
+            catch (MySqlException ex)
             {
-                throw new Exception($"An error occurred. Please try again later.\n\nError {ex.Number}: {ex.Message}");
+                throw new Exception($"An error occurred. Please try again later or contact an administrator.\n\nError Code: {ex.Number}");
             }
         }
 
-        public void AddNewEvent(EventDTO dto){
+        public void AddNewEvent(EventDTO dto)
+        {
             try
             {
                 using (MySqlConnection connection = new MySqlConnection(DatabaseClass.connectionString))
@@ -64,10 +67,11 @@ namespace KalenderApp.DAL
                 throw new Exception("An error occurred. Please try again later." + ex.Number);
             }
         }
-        public void EditEvent(EventDTO dto){
+        public void EditEvent(EventDTO dto)
+        {
             try
             {
-                using(MySqlConnection connection = new MySqlConnection(DatabaseClass.connectionString))
+                using (MySqlConnection connection = new MySqlConnection(DatabaseClass.connectionString))
                 {
                     string query = "UPDATE event SET category_id = @category_id, name = @name, start_time = @start_time, end_time = @end_time, location = @location, repetition = @repetition WHERE id = @id";
                     MySqlCommand command = new MySqlCommand(query, connection);
@@ -93,7 +97,7 @@ namespace KalenderApp.DAL
         {
             try
             {
-                using(MySqlConnection connection = new MySqlConnection(DatabaseClass.connectionString))
+                using (MySqlConnection connection = new MySqlConnection(DatabaseClass.connectionString))
                 {
                     string query = "DELETE FROM event WHERE id = @id";
                     MySqlCommand command = new MySqlCommand(query, connection);
